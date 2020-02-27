9a8760a9b835eebab51e504185d3f20814af748b
John Kester <nnamanikester@gmail.com>
2020-02-27 09:15:24 +0100

    Initial Commit

---
+++ app/components/App.vue
@@ -0,0 +1,67 @@
+<template>
+    <Page>
+        <ActionBar>
+            <GridLayout width="100%" columns="auto, *">
+                <Label text="MENU" @tap="$refs.drawer.nativeView.showDrawer()" col="0"/>
+                <Label class="title" text="NativeToDo"  col="1"/>
+            </GridLayout>
+        </ActionBar>
+
+        <RadSideDrawer ref="drawer">
+            <StackLayout ~drawerContent backgroundColor="#ffffff">
+                <Label class="drawer-header" text="Drawer"/>
+
+                <Label class="drawer-item" text="Home"/>
+                <Label class="drawer-item" text="About"/>
+                <Label class="drawer-item" text="Settings"/>
+            </StackLayout>
+
+            <GridLayout ~mainContent columns="*" rows="*">
+                <Label class="message" :text="msg" col="0" row="0"/>
+            </GridLayout>
+        </RadSideDrawer>
+    </Page>
+</template>
+
+<script >
+  export default {
+    data() {
+      return {
+        msg: 'Intersting! I love this shi*'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+    ActionBar {
+        background-color: #53ba82;
+        color: #ffffff;
+    }
+
+    .title {
+        text-align: left;
+        padding-left: 16;
+    }
+
+    .message {
+        vertical-align: center;
+        text-align: center;
+        font-size: 20;
+        color: #333333;
+    }
+
+    .drawer-header {
+        padding: 50 16 16 16;
+        margin-bottom: 16;
+        background-color: #53ba82;
+        color: #ffffff;
+        font-size: 24;
+    }
+
+    .drawer-item {
+        padding: 8 16;
+        color: #333333;
+        font-size: 16;
+    }
+</style>
