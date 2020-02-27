a90ea0aa03f6bfade130ab0cd527a7cfd507659d
John Kester <nnamanikester@gmail.com>
2020-02-27 09:25:15 +0100

    Added Vuex store

--- app/main.js
+++ app/main.js
@@ -1,6 +1,6 @@
 import Vue from 'nativescript-vue'
 import App from './components/App'
-import store from './store'
+import store from './store/index'
   
 // Prints Vue logs when --env.production is *NOT* set while building
 Vue.config.silent = (TNS_ENV === 'production')
